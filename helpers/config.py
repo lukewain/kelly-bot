bb94791690bc6d5f6ad98766a2723c0f9cd8d2a0
lukewain <lukewainhouse@outlook.com>
2024-09-16 22:51:43 +0100

    Updated helper files

--- helpers/config.py
+++ helpers/config.py
@@ -2,5 +2,11 @@ __all__ = ("Config",)
 
 from dataclasses import dataclass
 
+from typing import List
+
+
 @dataclass
-class Config
+class Config:
+    token: str
+    prefix: str
+    owner_ids: List[int]
